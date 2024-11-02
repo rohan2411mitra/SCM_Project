74820d1640dc16835836a863ff6dd8fbb24ea56f
Rohan Mitra <rohanmitra2420@gmail.com>
2024-11-02 13:16:37 +0530

    Just changing blank pages to something presentable

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,7 +1,14 @@
 package com.scm.controllers;
 
+import com.scm.entities.Contact;
+import com.scm.entities.User;
+import com.scm.helpers.Helper;
+import com.scm.services.ContactService;
+import com.scm.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +21,28 @@ import java.security.Principal;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private ContactService contactService;
+
     private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     // User Dashboard page
     @GetMapping(value = "/dashboard")
-    public String userDashboard(Principal principal) {
+    public String userDashboard(Principal principal,Model model,
+                                Authentication authentication) {
+        // Load all the user contacts
+        String email = Helper.getEmailOfLoggedInUser(authentication);
+
+        User user = userService.getUserByEmail(email);
+
+        Page<Contact> pageContact = contactService.getByUser(user, 0, 10, "name", "asc");
+        int totalFavorites = (int) pageContact.getContent().stream()
+                .filter(Contact::isFavorite)
+                .count();
+        model.addAttribute("totalFavorites", totalFavorites);
+        model.addAttribute("pageContact", pageContact);
         System.out.println("User dashboard");
         return "user/dashboard";
     }
